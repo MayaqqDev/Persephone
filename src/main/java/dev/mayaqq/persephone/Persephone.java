e751ab008d0fd0f183a549c99e65f6e29bcd2b32
Maximusbarcz <maxim.baranek@gmail.com>
2023-04-02 15:39:02 +0200

    Made compatible with origins (Holy shit)

--- src/main/java/dev/mayaqq/persephone/Persephone.java
+++ src/main/java/dev/mayaqq/persephone/Persephone.java
@@ -1,6 +1,14 @@
 package dev.mayaqq.persephone;
 
+import io.github.apace100.origins.networking.ModPackets;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,5 +23,6 @@ public class Persephone implements ModInitializer {
 
     public interface PlayerExtensions {
         void setJoinInvulnerabilityTicks(int ticks);
+
     }
 }
--- src/main/java/dev/mayaqq/persephone/mixin/OriginModPacketsC2SMixin.java
+++ src/main/java/dev/mayaqq/persephone/mixin/OriginModPacketsC2SMixin.java
@@ -1,27 +1,26 @@
 package dev.mayaqq.persephone.mixin;
 
 import dev.mayaqq.persephone.Persephone;
+import io.github.apace100.origins.networking.ModPacketsC2S;
+import io.github.apace100.origins.origin.Origin;
+import io.github.apace100.origins.origin.OriginLayer;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
-@Mixin(PlayerManager.class)
-abstract class PlayerManagerMixin {
-    @Inject(method = "onPlayerConnect", at = @At("HEAD"))
-    private void onPlayerConnect(ClientConnection connection, ServerPlayerEntity player, CallbackInfo ci) {
+@Mixin(ModPacketsC2S.class)
+public class OriginModPacketsC2SMixin {
+    @Inject(method = "confirmOrigin", at = @At("HEAD"))
+    private static void confirmOrigin(ServerPlayerEntity player, OriginLayer layer, Origin origin, CallbackInfo ci) {
         if (!player.getScoreboardTags().contains("firstSpawn")) {
             Persephone.PlayerExtensions playerExtensions = (Persephone.PlayerExtensions) player;
             playerExtensions.setJoinInvulnerabilityTicks(0);
             player.damage(firstSpawn(), 1000);
         }
     }
-
     private static DamageSource firstSpawn() {
         return (new DamageSource("firstSpawn").setBypassesArmor().setBypassesProtection().setUnblockable());
     }
